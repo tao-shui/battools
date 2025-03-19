c2c52bd5031524ce4f488e139accce148ef42401
tao-shui <541441967@qq.com>
2025-03-19 11:39:27 +0800

    updated

--- renamebytxtfile/rename.bat
+++ renamebytxtfile/rename.bat
@@ -1,19 +1,19 @@
 @echo off 1>nul
 chcp 936>nul 2>nul
-echo pls pull the folder here
-echo 拉取需要处理的文件夹
-set /p a=
-
+echo ^>^>^>pls pull the folder here
+echo ^>^>^>拉取需要处理的文件夹
+set /p a=^>^>^>
+rem type ex.txt
 for /f "tokens=1,*", %%i in (ex.txt) do (
-echo %a%\%%i
 if exist "%a%\%%j.jpg" (
 if not exist "%a%\%%i.jpg" (
 ren "%a%\%%j.jpg" "%%i.jpg"
+echo %%j.jpg --^> %%i.jpg
 ) else (
-echo %%i.jpg 图片已存在
+echo %a%\%%i.jpg 图片已存在
 )
 ) else (
-echo %%j.jpg 图片不存在
+echo %a%\%%j.jpg 图片不存在
 )
 )
 
