d54b57460d423818c8c34bd21f0325bd7bd5cbb4
tao-shui <541441967@qq.com>
2025-03-24 16:17:39 +0800

    fix the bug if press enter directly will be corrupted

--- renamebytxtfile/rename.bat
+++ renamebytxtfile/rename.bat
@@ -1,9 +1,13 @@
 @echo off 1>nul
 chcp 936>nul 2>nul
-echo ^>^>^>pls pull the folder here
-echo ^>^>^>拉取需要处理的文件夹
+echo ^>^>^>pls pull the folder here and press enter
+echo ^>^>^>or press enter directly to use current path
+echo ^>^>^>拉取需要处理的文件夹按回车启动
+echo ^>^>^>直接按回车将使用当前路径
 set /p a=^>^>^>
+if not defined a set a=%cd%
 if ^%a:~0,1%==^" set a=%a:~1,-1%
+echo %a%
 rem type ex.txt
 for /f "tokens=1,*", %%i in (ex.txt) do (
 if exist "%a%\%%j.jpg" (
@@ -21,4 +25,3 @@ echo %a%\%%j.jpg 图片不存在
 pause>nul
 
 rem for /f "tokens=1,2", %i in (ex.txt) do echo %i %j
-rem 需要填写配置文件ex.txt
