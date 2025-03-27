92bef284d4ebda6a356684da91bc13e643844f63
tao-shui <541441967@qq.com>
2025-03-27 11:17:59 +0800

    fixed a bug

--- renamebytxtfile/rename.bat
+++ renamebytxtfile/rename.bat
@@ -7,18 +7,18 @@ echo ^>^>^>直接按回车将使用当前路径
 set /p a=^>^>^>
 if not defined a set a=%cd%
 if ^%a:~0,1%==^" set a=%a:~1,-1%
-@REM echo %a%
+echo 当前路径：%a%
 rem type ex.txt
-for /f "tokens=1,*", %%i in (ex.txt) do (
+for /f "tokens=1,*" %%i in (ex.txt) do (
 if exist "%a%\%%j.jpg" (
 if not exist "%a%\%%i.jpg" (
 ren "%a%\%%j.jpg" "%%i.jpg"
 echo %%j.jpg --^> %%i.jpg
 ) else (
-echo %a%\%%i.jpg 图片已存在
+echo %%i.jpg 图片已存在
 )
 ) else (
-echo %a%\%%j.jpg 图片不存在
+echo %%j.jpg 图片不存在
 )
 )
 
